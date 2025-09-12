8f2621244a3c2f62aa150f473b001bb4dd3b29ee
Rael Cassimiro <raelkiluanji@gmail.com>
2025-09-12 07:56:24 -0300

    Lab01S03: protótipo funcional com validações, CSVs e comprovante

--- LABORATÓRIO 01 - Sistema de Matrículas/src/main/java/br/edu/univ/matriculas/domain/Usuario.java
+++ LABORATÓRIO 01 - Sistema de Matrículas/src/main/java/br/edu/univ/matriculas/domain/Usuario.java
@@ -1,10 +1,14 @@
 package br.edu.univ.matriculas.domain;
 
-public class Usuario {
-    private String id;
-    private String nome;
-    private String email;
-    private String senhaHash;
+public abstract class Usuario {
+  protected String id;
+  protected String nome;
+  protected String email;
 
-    // getters e setters
+  public String getId() { return id; }
+  public void setId(String id) { this.id = id; }
+  public String getNome() { return nome; }
+  public void setNome(String nome) { this.nome = nome; }
+  public String getEmail() { return email; }
+  public void setEmail(String email) { this.email = email; }
 }
